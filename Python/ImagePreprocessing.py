5a72e2c0775b55e1d7c076a603e3000f5a28cd1c
RamyGadalla <130904953+RamyGadalla@users.noreply.github.com>
2024-10-25 15:28:45 -0300

    Initial commit

---
+++ Python/ImagePreprocessing.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 28 16:13:21 2022
+
+@author: ramygadalla
+"""
+
+import shutil
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import List
+
+import pandas as pd
+
+import imcsegpipe
+from imcsegpipe.utils import sort_channels_by_mass
+
+
+# the paths with the ziped acquisition files
+raw_dirs = ["../raw"]
+raw_dirs = [Path(raw_dir) for raw_dir in raw_dirs]
+
+# regular expression to select files
+file_regex = "*Patient*.zip"
+
+# panel information
+panel_file = "../raw/panel.csv"
+panel_channel_col = "Metal Tag"
+panel_keep_col = "full"
+panel_ilastik_col = "ilastik"
+
+
+# working directory storing all outputs
+work_dir = "../analysis"
+work_dir = Path(work_dir)
+work_dir.mkdir(exist_ok=True)
+
+# general output directories
+acquisitions_dir = work_dir / "ometiff"
+ilastik_dir = work_dir / "ilastik"
+crops_dir = work_dir / "crops"
+cellprofiler_input_dir = work_dir / "cpinp"
+cellprofiler_output_dir = work_dir / "cpout"
+histocat_dir = work_dir / "histocat"
+
+# Final output directories
+final_images_dir = cellprofiler_output_dir / "images"
+final_masks_dir = cellprofiler_output_dir / "masks"
+final_probabilities_dir = cellprofiler_output_dir / "probabilities"
+
+# specified folder now created
+acquisitions_dir.mkdir(exist_ok=True)
+crops_dir.mkdir(exist_ok=True)
+ilastik_dir.mkdir(exist_ok=True)
+cellprofiler_input_dir.mkdir(exist_ok=True)
+cellprofiler_output_dir.mkdir(exist_ok=True)
+histocat_dir.mkdir(exist_ok=True)
+
+final_images_dir.mkdir(exist_ok=True)
+final_masks_dir.mkdir(exist_ok=True)
+final_probabilities_dir.mkdir(exist_ok=True)
+
+
+temp_dirs: List[TemporaryDirectory] = []
+
+try:
+    for raw_dir in raw_dirs:
+        zip_files = list(raw_dir.rglob(file_regex))
+        if len(zip_files) > 0:
+            temp_dir = TemporaryDirectory()
+            temp_dirs.append(temp_dir)
+            for zip_file in sorted(zip_files):
+                imcsegpipe.extract_zip_file(zip_file, temp_dir.name)
+    acquisition_metadatas = []
+    for raw_dir in raw_dirs + [Path(temp_dir.name) for temp_dir in temp_dirs]:
+        mcd_files = list(raw_dir.rglob("*.mcd"))
+        if len(mcd_files) > 0:
+            txt_files = list(raw_dir.rglob("*.txt"))
+            matched_txt_files = imcsegpipe.match_txt_files(mcd_files, txt_files)
+            for mcd_file in mcd_files:
+                acquisition_metadata = imcsegpipe.extract_mcd_file(
+                    mcd_file,
+                    acquisitions_dir / mcd_file.stem,
+                    txt_files=matched_txt_files[mcd_file],
+                )
+                acquisition_metadatas.append(acquisition_metadata)
+    acquisition_metadata = pd.concat(acquisition_metadatas, copy=False)
+    acquisition_metadata.to_csv(cellprofiler_input_dir / "acquisition_metadata.csv")
+finally:
+    for temp_dir in temp_dirs:
+        temp_dir.cleanup()
+    del temp_dirs
---
+++ Python/download_data_script.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 27 16:38:44 2022
+
+@author: ramygadalla
+"""
+
+#remember to set the env to 'imcsegpipe'
+#$ conda activate imcsegpipe - in terminal. this environment is in 'anaconda3/envs'
+#should later move to the project folder.
+
+
+# Load necessary modules
+from pathlib import Path
+from urllib import request
+
+#create path object using pathlib module. ".." means parent directory
+raw_folder = Path("../raw")
+#make a directory in the specified path
+raw_folder.mkdir(exist_ok=True, parents=True)
+
+for example_file_name, example_file_url in [
+    (
+        "Patient1.zip",
+        "https://zenodo.org/record/5949116/files/Patient1.zip",
+    ),
+    (
+        "Patient2.zip",
+        "https://zenodo.org/record/5949116/files/Patient2.zip",
+    ),
+    (
+        "Patient3.zip",
+        "https://zenodo.org/record/5949116/files/Patient3.zip",
+    ),
+    (
+        "Patient4.zip",
+        "https://zenodo.org/record/5949116/files/Patient4.zip",
+    ),
+    (
+        "panel.csv",
+        "https://zenodo.org/record/5949116/files/panel.csv",
+    )
+]:
+    example_file = raw_folder / example_file_name
+    if not example_file.exists():
+        request.urlretrieve(example_file_url, example_file)
+
+
+# Ilastik project
+ilastik_project = Path("..") / "IMCWorkflow.ilp"
+if not ilastik_project.exists():
+    request.urlretrieve("https://zenodo.org/record/6449127/files/IMCWorkflow.ilp", ilastik_project)
+
+# Sample metadata
+sample_metadata = Path("..") / "sample_metadata.xlsx"
+if not sample_metadata.exists():
+    request.urlretrieve("https://zenodo.org/record/5949116/files/sample_metadata.xlsx", sample_metadata)
